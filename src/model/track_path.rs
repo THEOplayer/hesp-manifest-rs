9abb6aef531e7dfa6623fa2700eab780239cac71
Christopher <christopher.leyssen@theoplayer.com>
2021-08-16 10:07:10 +0200

    getters

--- src/model/track_path.rs
+++ src/model/track_path.rs
@@ -13,7 +13,7 @@ pub struct TrackPathRef<'a> {
     presentation_id: &'a str,
     switching_set_id: &'a str,
     media_type: MediaType,
-    track_id: &'a str
+    track_id: &'a str,
 }
 
 impl TrackPath {
@@ -25,6 +25,11 @@ impl TrackPath {
     ) -> Self {
         Self { presentation_id, switching_set_id, media_type, track_id }
     }
+
+    pub fn presentation_id(&self) -> &str { &self.presentation_id }
+    pub fn switching_set_id(&self) -> &str { &self.switching_set_id }
+    pub fn media_type(&self) -> MediaType { self.media_type }
+    pub fn track_id(&self) -> &str { &self.track_id }
 }
 
 impl<'a> TrackPathRef<'a> {
@@ -36,4 +41,9 @@ impl<'a> TrackPathRef<'a> {
     ) -> Self {
         Self { presentation_id, switching_set_id, media_type, track_id }
     }
+
+    pub fn presentation_id(&self) -> &'a str { self.presentation_id }
+    pub fn switching_set_id(&self) -> &'a str { self.switching_set_id }
+    pub fn media_type(&self) -> MediaType { self.media_type }
+    pub fn track_id(&self) -> &'a str { self.track_id }
 }

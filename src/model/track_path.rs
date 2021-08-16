d7d7a3178d9dcf8a4bb01f1f26b4dfe72888e408
Christopher <christopher.leyssen@theoplayer.com>
2021-08-16 16:28:39 +0200

    fix path order (media_type before switching_set_id)

--- src/model/track_path.rs
+++ src/model/track_path.rs
@@ -6,24 +6,24 @@ use itertools::Itertools;
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct TrackPath {
     presentation_id: String,
-    switching_set_id: String,
     media_type: MediaType,
+    switching_set_id: String,
     track_id: String,
 }
 
 impl TrackPath {
     pub fn new(
         presentation_id: String,
-        switching_set_id: String,
         media_type: MediaType,
+        switching_set_id: String,
         track_id: String,
     ) -> Self {
-        Self { presentation_id, switching_set_id, media_type, track_id }
+        Self { presentation_id, media_type, switching_set_id, track_id }
     }
 
     pub fn presentation_id(&self) -> &str { &self.presentation_id }
-    pub fn switching_set_id(&self) -> &str { &self.switching_set_id }
     pub fn media_type(&self) -> MediaType { self.media_type }
+    pub fn switching_set_id(&self) -> &str { &self.switching_set_id }
     pub fn track_id(&self) -> &str { &self.track_id }
 }
 
@@ -31,7 +31,7 @@ impl fmt::Display for TrackPath {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
             f, "{}/{}/{}/{}",
-            self.presentation_id, self.switching_set_id, self.media_type, self.track_id,
+            self.presentation_id, self.media_type, self.switching_set_id, self.track_id,
         )
     }
 }
@@ -39,15 +39,15 @@ impl fmt::Display for TrackPath {
 impl FromStr for TrackPath {
     type Err = Error;
     fn from_str(input: &str) -> Result<Self> {
-        let (presentation_id, switching_set_id, media_type, track_id) = input
+        let (presentation_id, media_type,switching_set_id, track_id) = input
             .split('/')
             .collect_tuple()
             .ok_or_else(|| Error::InvalidTrackPath(input.to_owned()))?;
         Ok(TrackPath::new(
             presentation_id.to_owned(),
-            switching_set_id.to_owned(),
             media_type.parse()?,
-            track_id.to_owned()
+            switching_set_id.to_owned(),
+            track_id.to_owned(),
         ))
     }
 }

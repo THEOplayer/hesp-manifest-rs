885608020672cd98c6f6dcf43de4b9eb00845eb9
Egon Okerman <dev@egon.me>
2021-12-08 15:08:48 +0100

    Restructure segment.rs file to be more consistent
    (struct -> impl -> trait impls)

--- src/model/segment.rs
+++ src/model/segment.rs
@@ -13,69 +13,9 @@ use crate::{Error, Result, ScaledDuration, TimeBounds};
 #[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Hash)]
 pub struct SegmentId(#[serde(deserialize_with = "UInt::deserialize_u64")] u64);
 
-impl fmt::Display for SegmentId {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        fmt::Display::fmt(&self.0, f)
-    }
-}
-
-#[skip_serializing_none]
-#[derive(Deserialize, Debug, Serialize, Clone)]
-#[serde(rename_all = "camelCase")]
-pub struct Segment {
-    id: SegmentId,
-    time_bounds: Option<TimeBounds>,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize, Default)]
-#[serde(try_from = "Vec<Segment>")]
-pub struct Segments(Vec<Segment>);
-
 impl SegmentId {
     pub fn next(self) -> Self {
         self + 1
-    }
-}
-
-impl TryFrom<Vec<Segment>> for Segments {
-    type Error = Error;
-
-    fn try_from(vec: Vec<Segment>) -> Result<Self> {
-        let jump = vec
-            .iter()
-            .map(Segment::id)
-            .tuple_windows()
-            .find(|&(a, b)| a.next() != b);
-        if let Some((a, b)) = jump {
-            Err(Error::InvalidSegmentIds(a, b))
-        } else {
-            Ok(Self(vec))
-        }
-    }
-}
-
-impl Deref for Segments {
-    type Target = [Segment];
-    fn deref(&self) -> &[Segment] {
-        &self.0
-    }
-}
-
-impl Borrow<[Segment]> for Segments {
-    fn borrow(&self) -> &[Segment] {
-        &self.0
-    }
-}
-
-impl Segment {
-    pub const fn id(&self) -> SegmentId {
-        self.id
-    }
-    pub fn duration(&self) -> Option<ScaledDuration> {
-        self.time_bounds?.duration()
-    }
-    pub const fn has_time_bounds(&self) -> bool {
-        self.time_bounds.is_some()
     }
 }
 
@@ -102,6 +42,66 @@ impl AddAssign<u64> for SegmentId {
 impl SubAssign<u64> for SegmentId {
     fn sub_assign(&mut self, rhs: u64) {
         self.0 -= rhs;
+    }
+}
+
+impl fmt::Display for SegmentId {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt::Display::fmt(&self.0, f)
+    }
+}
+
+#[skip_serializing_none]
+#[derive(Deserialize, Debug, Serialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct Segment {
+    id: SegmentId,
+    time_bounds: Option<TimeBounds>,
+}
+
+impl Segment {
+    pub const fn id(&self) -> SegmentId {
+        self.id
+    }
+    pub fn duration(&self) -> Option<ScaledDuration> {
+        self.time_bounds?.duration()
+    }
+    pub const fn has_time_bounds(&self) -> bool {
+        self.time_bounds.is_some()
+    }
+}
+
+impl TryFrom<Vec<Segment>> for Segments {
+    type Error = Error;
+
+    fn try_from(vec: Vec<Segment>) -> Result<Self> {
+        let jump = vec
+            .iter()
+            .map(Segment::id)
+            .tuple_windows()
+            .find(|&(a, b)| a.next() != b);
+        if let Some((a, b)) = jump {
+            Err(Error::InvalidSegmentIds(a, b))
+        } else {
+            Ok(Self(vec))
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Default)]
+#[serde(try_from = "Vec<Segment>")]
+pub struct Segments(Vec<Segment>);
+
+impl Deref for Segments {
+    type Target = [Segment];
+    fn deref(&self) -> &[Segment] {
+        &self.0
+    }
+}
+
+impl Borrow<[Segment]> for Segments {
+    fn borrow(&self) -> &[Segment] {
+        &self.0
     }
 }
 

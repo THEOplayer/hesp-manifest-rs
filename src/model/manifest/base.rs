257718e503ca6c030824b12a59170299f7819bdf
Jeroen Tempels <jeroen.tempels@theoplayer.com>
2024-01-24 13:26:49 +0100

    Remove manifest trait

--- src/model/manifest/base.rs
+++ src/model/manifest/base.rs
@@ -4,8 +4,9 @@ use url::Url;
 
 use crate::util::{Entity, EntityIter, EntityIterMut, EntityMap, FromEntities};
 use crate::{
-    Address, AudioTrack, Error, LiveStream, ManifestData, ManifestDeserialize, ManifestSerialize,
-    MetadataTrack, Presentation, Result, StreamType, VideoTrack,
+    Address, AudioTrack, Error, InitializableTrack, LiveStream, ManifestData, ManifestDeserialize,
+    ManifestSerialize, MetadataTrack, Presentation, Result, StreamType, Track, TrackUid,
+    VideoTrack,
 };
 
 #[derive(Debug, Clone, Serialize)]
@@ -79,6 +80,47 @@ impl BaseManifest {
         self.presentations.get_mut(id)
     }
 
+    #[must_use]
+    pub fn track(&self, track_uid: &TrackUid) -> Option<&dyn Track> {
+        self.presentation(track_uid.presentation_id())?.track(
+            track_uid.media_type(),
+            track_uid.switching_set_id(),
+            track_uid.track_id(),
+        )
+    }
+
+    #[must_use]
+    pub fn track_mut(&mut self, track_uid: &TrackUid) -> Option<&mut dyn Track> {
+        self.presentation_mut(track_uid.presentation_id())?
+            .track_mut(
+                track_uid.media_type(),
+                track_uid.switching_set_id(),
+                track_uid.track_id(),
+            )
+    }
+
+    #[must_use]
+    pub fn initializable_track(&self, track_uid: &TrackUid) -> Option<&dyn InitializableTrack> {
+        self.presentation(track_uid.presentation_id())?
+            .initializable_track(
+                track_uid.media_type(),
+                track_uid.switching_set_id(),
+                track_uid.track_id(),
+            )
+    }
+
+    pub fn initializable_track_mut(
+        &mut self,
+        track_uid: &TrackUid,
+    ) -> Option<&mut dyn InitializableTrack> {
+        self.presentation_mut(track_uid.presentation_id())?
+            .initializable_track_mut(
+                track_uid.media_type(),
+                track_uid.switching_set_id(),
+                track_uid.track_id(),
+            )
+    }
+
     pub fn from_json(location: Url, json: &str) -> Result<Self> {
         let deserializer = &mut serde_json::Deserializer::from_str(json);
         let data = match serde_path_to_error::deserialize(deserializer)? {
@@ -90,7 +132,7 @@ impl BaseManifest {
     }
 }
 
-pub(super) fn validate_active(
+fn validate_active(
     stream_type: &StreamType,
     presentations: &EntityMap<Presentation>,
 ) -> Result<()> {
--- src/model/manifest/mod.rs
+++ src/model/manifest/mod.rs
@@ -1,66 +1,7 @@
-use chrono::{DateTime, FixedOffset};
-use url::Url;
-
 pub use base::*;
 pub use data::*;
 pub use stream::*;
 
-use crate::util::{EntityIter, EntityIterMut};
-use crate::{InitializableTrack, Presentation, Result, Track, TrackUid};
-
 mod base;
 mod data;
 mod stream;
-
-pub trait Manifest: Sized {
-    fn new(location: Url, data: ManifestData) -> Result<Self>;
-    fn presentations(&self) -> EntityIter<Presentation>;
-    fn presentations_mut(&mut self) -> EntityIterMut<Presentation>;
-    fn presentation(&self, id: &str) -> Option<&Presentation>;
-    fn presentation_mut(&mut self, id: &str) -> Option<&mut Presentation>;
-    fn stream_type(&self) -> &StreamType;
-
-    fn track(&self, track_uid: &TrackUid) -> Option<&dyn Track> {
-        self.presentation(track_uid.presentation_id())?.track(
-            track_uid.media_type(),
-            track_uid.switching_set_id(),
-            track_uid.track_id(),
-        )
-    }
-
-    fn track_mut(&mut self, track_uid: &TrackUid) -> Option<&mut dyn Track> {
-        self.presentation_mut(track_uid.presentation_id())?
-            .track_mut(
-                track_uid.media_type(),
-                track_uid.switching_set_id(),
-                track_uid.track_id(),
-            )
-    }
-
-    fn initializable_track(&self, track_uid: &TrackUid) -> Option<&dyn InitializableTrack> {
-        self.presentation(track_uid.presentation_id())?
-            .initializable_track(
-                track_uid.media_type(),
-                track_uid.switching_set_id(),
-                track_uid.track_id(),
-            )
-    }
-
-    fn initializable_track_mut(
-        &mut self,
-        track_uid: &TrackUid,
-    ) -> Option<&mut dyn InitializableTrack> {
-        self.presentation_mut(track_uid.presentation_id())?
-            .initializable_track_mut(
-                track_uid.media_type(),
-                track_uid.switching_set_id(),
-                track_uid.track_id(),
-            )
-    }
-
-    fn creation_date(&self) -> DateTime<FixedOffset>;
-
-    fn fallback_poll_rate(&self) -> u64;
-
-    fn from_json(location: Url, json: &str) -> Result<Self>;
-}

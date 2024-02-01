81195ebc0a7befaa65d36a3e6b8d7651adeae638
Christopher <christopher.leyssen@theoplayer.com>
2024-02-01 15:34:20 +0100

    Pretty prent example

--- examples/json.rs
+++ examples/json.rs
@@ -8,8 +8,8 @@ fn main() -> anyhow::Result<()> {
     let location = Url::parse("http://localhost")?;
     let input = fs::read_to_string("tests/v2_0_0-manifest.json")?;
     let manifest = Manifest::from_json(location, &input)?;
-    let json = serde_json::to_string(&manifest)?;
+    let json = serde_json::to_string_pretty(&manifest)?;
 
-    println!("{json:#?}");
+    println!("{json}");
     Ok(())
 }
